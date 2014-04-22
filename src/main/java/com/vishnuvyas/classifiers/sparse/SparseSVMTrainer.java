baada9267a51b7d01bbbb53722ce70443303d189
Vishnu Vyas <vishnuvyas@gmail.com>
2014-04-21 23:43:41 -0700

    Sparse SVM
    1. An implementation of sparse hinge loss (so we can compute hinge loss
       on sparse datasets.
    
    2. An implementation of sparse svm classifier that can use sparse
       datasets (and features).
    
    3. An implementation of a trainer over sparse datasets which simply uses
       our sparse hinge-loss to generate a sparse classifier and a spec to go
       along with it (which is evaluated against dense svm trainer).

---
+++ src/main/java/com/vishnuvyas/classifiers/sparse/SparseSVMTrainer.java
@@ -0,0 +1,59 @@
+package com.vishnuvyas.classifiers.sparse;
+
+import com.vishnuvyas.classifiers.Classifier;
+import com.vishnuvyas.classifiers.svm.SVMTrainer;
+import com.vishnuvyas.classifiers.training.Trainer;
+import com.vishnuvyas.classifiers.training.dataset.Dataset;
+import com.vishnuvyas.optimize.GradientDescentOptimizer;
+
+import java.util.Arrays;
+
+/**
+ * Created by vishnu on 4/21/14.
+ */
+public class SparseSVMTrainer implements Trainer<SparseVector,Boolean> {
+    private double batchFraction;
+    private int niter;
+    private double regularizationParameter;
+    private double learningRate;
+    private boolean printing=false;
+
+    public void setPrinting(boolean p) {
+        printing = p;
+    }
+
+
+    /**
+     * creates a new sparse svm trainer
+     * @param niter the number of iterations to run this trainer
+     * @param batchFraction the fraction of the data that is used in each iteration
+     * @param regularizationParameter the regularization parameter
+     */
+    public SparseSVMTrainer(int niter,double batchFraction, double regularizationParameter,double learningRate) {
+        this.niter = niter;
+        this.batchFraction = batchFraction;
+        this.regularizationParameter = regularizationParameter;
+        this.learningRate = learningRate;
+    }
+
+    @Override
+    public Classifier<SparseVector, Boolean> train(Dataset<SparseVector, Boolean> dataset) {
+        GradientDescentOptimizer optimizer = new GradientDescentOptimizer();
+        double [] w = new double[dataset.dim()];
+        Arrays.fill(w,0.0d);
+
+        for(int t = 0; t < niter; ++t) {
+            Dataset<SparseVector,Boolean> sample = dataset.sample(batchFraction);
+            SparseHingeLoss objectiveFunction = new SparseHingeLoss(sample,regularizationParameter);
+
+            if(t%1000==0 && t > 0 && printing) {
+                System.out.println("The weights at step " + t
+                        + " are " + SVMTrainer.printWeights(w)
+                        + " and loss is " + String.format("%.3f", objectiveFunction.eval(w)));
+            }
+
+            w = optimizer.descentStep(objectiveFunction,w,learningRate,t);
+        }
+        return new SparseSVMClassifier(w);
+    }
+}
---
+++ src/test/scala/com/vishnuvyas/classifiers/sparse/SparseSVMTrainerSpec.scala
@@ -0,0 +1,27 @@
+package com.vishnuvyas.classifiers.sparse
+
+import org.scalatest.{ShouldMatchers, FlatSpec}
+import com.vishnuvyas.classifiers.svm.{SVMTrainer, DiabetesDataset}
+
+/**
+ * Created by vishnu on 4/21/14.
+ */
+class SparseSVMTrainerSpec extends FlatSpec with ShouldMatchers {
+
+  "A SparseSVMTrainer" should "be able to reproduce the same results as a regular svm trainer" in {
+    val dataset = new DiabetesDataset
+    val sparseDataset = new SparseDatasetAdapter(dataset)
+
+    val trainer = new SVMTrainer(4000,1,0.01)
+    val sparseTrainer = new SparseSVMTrainer(4000,1,0.01,0.1)
+
+    val classifier = trainer.train(dataset)
+    val sparseClassifer = sparseTrainer.train(sparseDataset)
+
+    val trainingError = dataset.trainingError(classifier)
+    val sparseTrainingError = sparseDataset.trainingError(sparseClassifer)
+
+    trainingError should be(sparseTrainingError)
+  }
+
+}
